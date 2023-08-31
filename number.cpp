1ef0a451f24552515c8ced2476f6f0adf41ab776
tushar shinde033 <tusharshinde0330@gmail.com>
2023-08-31 10:57:53 +0530

    Number Guessing Game

---
+++ number.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main()
+{
+    srand(static_cast<unsigned int>(time(nullptr)));
+    int targetNumber = rand() % 100 + 1;
+
+    int userGuess = 0;
+    int attempts = 0;
+
+    while (userGuess != targetNumber)
+    {
+        cout << "Guess the number between 1 and 100: ";
+        cin >> userGuess;
+        attempts++;
+
+        if (userGuess < targetNumber)
+        {
+            cout << "Too low! Try again." << endl;
+        }
+        else if (userGuess > targetNumber)
+        {
+            cout << "Too high! Try again." << endl;
+        }
+    }
+
+    cout << "Congratulations! You guessed the correct number " << targetNumber << " in " << attempts << " attempts!" << endl;
+
+    return 0;
+}
